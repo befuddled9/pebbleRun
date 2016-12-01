dd9ef7fa7baa50ab123b1da7c6dd63fc6aba3243
John Hildebrand <befuddled9@users.noreply.github.com>
2016-12-01 16:55:53 -0500

    Cadence!
    Changed to have cadence worker instead of steps.
    Returns steps/2 per minute.

--- worker_src/c/cadence.c
+++ worker_src/c/cadence.c
@@ -3,12 +3,10 @@
 #define WORKER_TICKS 0
 
 static uint16_t s_ticks = 0;
-static time_t start_time;
 static time_t last_time;
 
 static void tick_handler(struct tm *tick_timer, TimeUnits units_changed) {
-  int numRcvd = 0;
-  
+  uint32_t numRcvd = 0;
   HealthMinuteData minute_data;
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Tick Handler: Enter");
     
@@ -16,29 +14,19 @@ static void tick_handler(struct tm *tick_timer, TimeUnits units_changed) {
   s_ticks++;
   
   // Get the number of steps since start time
-  time_t time_now = time(NULL);
-  char time_str[80];
-  strftime(time_str, 80, "%T", localtime(&last_time));
-  APP_LOG(APP_LOG_LEVEL_INFO, "Last time : %s", time_str);
-  
-  strftime(time_str, 80, "%T", localtime(&time_now));
-  APP_LOG(APP_LOG_LEVEL_INFO, "Time now: %s", time_str);
-  
+  time_t time_now = time(NULL);  
   numRcvd = health_service_get_minute_history(&minute_data, 1, &last_time, &time_now);
   last_time = time(NULL);
   
-  APP_LOG(APP_LOG_LEVEL_INFO, "numRcvd: %d", numRcvd);
-  APP_LOG(APP_LOG_LEVEL_INFO, "Steps: %d", minute_data.steps);
-
-  if (minute_data.is_invalid) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Health data is invalid!");
+  if (numRcvd == 0 || minute_data.is_invalid) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "No health data to use");
     goto done;
   }
   
   // Construct a data packet
   AppWorkerMessage msg_data = {
     .data0 = s_ticks,
-    .data1 = minute_data.steps
+    .data1 = (minute_data.steps/2)
   };
 
   // Send the data to the foreground app
@@ -49,7 +37,6 @@ done:
 }
 
 static void worker_init() {
-  start_time = time(NULL);
   last_time = time(NULL);
   // Use the TickTimer Service as a data source
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
